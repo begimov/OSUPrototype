06e60ed6a570d954f7487f04b4dac03a6804b528
begimov <begimov@gmail.com>
2017-09-26 10:51:10 +0300

    ui updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,13 +6,9 @@
       <div class="col-md-12">
         <div class="panel panel-default">
           <div class="panel-body">
-            <ul class="nav nav-tabs">
-              <li role="presentation">
-                <router-link :to="{ name: 'search-panel' }">В каталоге</router-link>
-              </li>
-              <li role="presentation">
-                <router-link :to="{ name: 'map-panel' }">На карте</router-link>
-              </li>
+            <ul class="list-inline">
+              <li><h4><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span>&nbsp;<router-link :to="{ name: 'search-panel' }">В КАТАЛОГЕ</router-link></h4></li>
+              <li><h4><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>&nbsp;<router-link :to="{ name: 'map-panel' }">НА КАРТЕ</router-link></h4></li>
             </ul>
             <router-view></router-view>
             <div id="map"></div>
